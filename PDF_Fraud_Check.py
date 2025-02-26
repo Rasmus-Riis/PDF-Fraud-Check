7e54993d2a7ed3299424627bdbb0fbb40ba63c6b
Rasmus Riis Kristensen <riisras@gmail.com>
2025-02-26 09:58:15 +0100

    Update PDF_Fraud_Check.py
    removed initial comment and put in readme

--- PDF_Fraud_Check.py
+++ PDF_Fraud_Check.py
@@ -1,24 +1,3 @@
-###########################################################################################
-#
-# Author: Rasmus Riis Kristensen
-# Contact: riisras((at))gmail.com
-# Name: PDF_Fraud_Check.py
-# Usage: Execute the script in a folder containing PDF files. Altered PDF files
-# will be put in subfolders to each examined folder in a folder named 'Altered files'.
-# The script is recursive.
-# Created: 11-11-2022
-# Modified: 17-02-2025
-# Usage: Execute the script directly with no arguments in the top folder.
-# (python PDS_Fraud_Detector.py) It will then look for PDF files in the
-# folder and subfolders in which the script resides and check if they have been altered
-# using Adobe Acrobat Pro. The EXIFTool will also have to be placed in the same directory
-# as the script. If run on a Linux machine then the script will have to be changed 
-# accordingly or the part with EXIFTool will have to be commented out
-#
-# Remember to open the TSV file with Excel
-# Version: 2.3.1
-###########################################################################################
-
 import os
 import subprocess
 import hashlib
